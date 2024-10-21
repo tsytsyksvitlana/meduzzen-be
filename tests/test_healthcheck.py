1f1508a8016b823d973bc26d0255bdaaca94c9ba
Svitlana Tsytsyk <TSYTSYK.svetlana@gmail.com>
2024-10-21 17:09:36 +0300

    fix status in tests

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -1,8 +1,11 @@
+from fastapi import status
+
+
 def test_read_healthcheck(client):
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == {
-        "status_code": 200,
+        "status_code": status.HTTP_200_OK,
         "detail": "ok",
         "result": "working",
     }
