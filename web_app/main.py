23301757542fc46350a1990b78c725c9c07c4585
Svitlana Tsytsyk <TSYTSYK.svetlana@gmail.com>
2024-10-22 20:23:58 +0300

    fix

--- web_app/main.py
+++ web_app/main.py
@@ -13,27 +13,26 @@ from web_app.routers.healthcheck import router as router
 logger = logging.getLogger(__name__)
 
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     logger.info("Starting up...")
-#
-#     await postgres_helper.engine.connect()
-#     logger.info("PostgreSQL connected.")
-#
-#     await redis_helper.redis.ping()
-#     logger.info("Redis connected.")
-#
-#     yield
-#
-#     await postgres_helper.dispose()
-#     logger.info("PostgreSQL connection closed.")
-#
-#     await redis_helper.close()
-#     logger.info("Redis connection closed.")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Starting up...")
+
+    await postgres_helper.engine.connect()
+    logger.info("PostgreSQL connected.")
+
+    await redis_helper.redis.ping()
+    logger.info("Redis connected.")
+
+    yield
+
+    await postgres_helper.dispose()
+    logger.info("PostgreSQL connection closed.")
+
+    await redis_helper.close()
+    logger.info("Redis connection closed.")
 
 
-# app = FastAPI(lifespan=lifespan)
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(router)
 
 origins = [
@@ -51,8 +50,9 @@ app.add_middleware(
 
 if __name__ == "__main__":
     uvicorn.run(
-        app,
+        "web_app.main:app",
         host=config.server_host,
         port=config.server_port,
-        reload=config.reload,
+        reload=False,
+        workers=2
     )
