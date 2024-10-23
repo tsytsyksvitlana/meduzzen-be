63846eb23a1ac2bcc3bb717719722cf15ea5fe1c
Svitlana Tsytsyk <TSYTSYK.svetlana@gmail.com>
2024-10-23 11:48:47 +0300

    added user schemas

--- web_app/migrations/versions/2024_10_23_0740-822686fc4e34_user_model_added.py
+++ web_app/migrations/versions/2024_10_23_0740-822686fc4e34_user_model_added.py
@@ -1,22 +1,21 @@
 """user model added
 
 Revision ID: 822686fc4e34
-Revises: 
+Revises:
 Create Date: 2024-10-23 07:40:35.397510
 
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "822686fc4e34"
-down_revision: Union[str, None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
