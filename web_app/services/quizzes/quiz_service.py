1c1d1c54a67ca3f10c0a02ce712a3ee7b4b31f1c
Svitlana Tsytsyk <TSYTSYK.svetlana@gmail.com>
2024-11-07 12:42:28 +0200

    added delete quiz functionality

--- web_app/services/quizzes/quiz_service.py
+++ web_app/services/quizzes/quiz_service.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from web_app.db.postgres_helper import postgres_helper as pg_helper
 from web_app.exceptions.companies import CompanyNotFoundException
 from web_app.exceptions.permission import PermissionDeniedException
-from web_app.exceptions.quizzes import QuizNotFoundException
+from web_app.exceptions.quizzes import QuizNotFoundException, QuestionNotFoundException
 from web_app.exceptions.validation import InvalidFieldException
 from web_app.models import User
 from web_app.models.answer import Answer
@@ -16,7 +16,7 @@ from web_app.repositories.company_membership_repository import (
 from web_app.repositories.company_repository import CompanyRepository
 from web_app.repositories.question_repository import QuestionRepository
 from web_app.repositories.quiz_repository import QuizRepository
-from web_app.schemas.quiz import QuizCreate
+from web_app.schemas.quiz import QuestionCreate, QuizCreate, QuizUpdate
 from web_app.schemas.roles import Role
 
 
@@ -52,6 +52,12 @@ class QuizService:
                 "The quiz must have at least two questions."
             )
 
+        for question_data in quiz_data.questions:
+            if len(question_data.answers) < 2:
+                raise InvalidFieldException(
+                    f"Question '{question_data.title}' must have at least two answers."
+                )
+
         quiz = Quiz(
             title=quiz_data.title,
             description=quiz_data.description,
@@ -63,17 +69,29 @@ class QuizService:
             question = Question(title=question_data.title, quiz_id=quiz.id)
             quiz.questions.append(question)
 
-            for answer_data in question_data.answers:
-                answer = Answer(
-                    text=answer_data.text,
-                    is_correct=answer_data.is_correct,
-                    question_id=question.id
-                )
-                question.answers.append(answer)
+            question.answers = [
+                Answer(text=answer_data.text, is_correct=answer_data.is_correct)
+                for answer_data in question_data.answers
+            ]
 
         await self.quiz_repository.create_obj(quiz)
         await self.quiz_repository.session.commit()
 
+        return quiz
+
+    async def update_quiz(self, quiz_id: int, quiz_data: QuizUpdate, current_user: User):
+        quiz = await self.quiz_repository.get_obj_by_id(quiz_id)
+        if not quiz:
+            raise QuizNotFoundException(quiz_id)
+
+        await self.check_is_owner_or_admin(quiz.company_id, current_user)
+
+        quiz.title = quiz_data.title
+        quiz.description = quiz_data.description
+        quiz.participation_frequency = quiz_data.participation_frequency
+
+        await self.quiz_repository.update_obj(quiz)
+        await self.quiz_repository.session.commit()
         return quiz
 
     async def get_quizzes_for_company(self, company_id, skip, limit):
@@ -91,6 +109,56 @@ class QuizService:
         await self.check_is_owner_or_admin(quiz.company_id, current_user)
 
         await self.quiz_repository.delete_obj(quiz_id)
+        await self.quiz_repository.session.commit()
+
+    async def add_question_to_quiz(
+        self,
+        quiz_id: int,
+        question_data: QuestionCreate,
+        current_user: User
+    ):
+        quiz = await self.quiz_repository.get_obj_by_id(quiz_id)
+        if not quiz:
+            raise QuizNotFoundException(quiz_id)
+        await self.check_is_owner_or_admin(quiz.company_id, current_user)
+
+        if len(question_data.answers) < 2:
+            raise InvalidFieldException(
+                f"Question '{question_data.title}' must have at least two answers."
+            )
+
+        question = Question(title=question_data.title, quiz_id=quiz_id)
+        await self.question_repository.create_obj(question)
+
+        question.answers = [
+            Answer(text=answer_data.text, is_correct=answer_data.is_correct)
+            for answer_data in question_data.answers
+        ]
+
+        await self.quiz_repository.session.commit()
+
+        return question
+
+    async def delete_question_from_quiz(
+        self,
+        quiz_id: int,
+        question_id: int,
+        current_user: User
+    ):
+        quiz = await self.quiz_repository.get_obj_by_id(quiz_id)
+        if not quiz:
+            raise QuizNotFoundException(quiz_id)
+        await self.check_is_owner_or_admin(quiz.company_id, current_user)
+
+        question = await self.question_repository.get_obj_by_id(question_id)
+        if not question or question.quiz_id != quiz.id:
+            raise QuestionNotFoundException(question_id)
+
+        remaining_questions = await self.question_repository.get_questions_for_quiz(question.quiz_id)
+        if len(remaining_questions) <= 2:
+            raise InvalidFieldException("A quiz must have at least 2 questions.")
+
+        await self.question_repository.delete_obj(question_id)
         await self.quiz_repository.session.commit()
 
 
