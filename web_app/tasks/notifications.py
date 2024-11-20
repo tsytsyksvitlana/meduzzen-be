6a24997d07d4d900a0992c595fe35a7667a8c306
Svitlana Tsytsyk <TSYTSYK.svetlana@gmail.com>
2024-11-20 12:35:15 +0200

    fix

--- web_app/tasks/notifications.py
+++ web_app/tasks/notifications.py
@@ -1,11 +1,15 @@
 import logging
 
+from web_app.db.postgres_helper import postgres_helper as pg_helper
 from web_app.repositories.company_repository import CompanyRepository
 from web_app.repositories.notification_repository import NotificationRepository
-from web_app.repositories.quiz_participation_repository import QuizParticipationRepository
+from web_app.repositories.quiz_participation_repository import (
+    QuizParticipationRepository
+)
 from web_app.repositories.quiz_repository import QuizRepository
-from web_app.services.notification.notification_service import NotificationService
-from web_app.db.postgres_helper import postgres_helper as pg_helper
+from web_app.services.notification.notification_service import (
+    NotificationService
+)
 
 logger = logging.getLogger(__name__)
 
@@ -15,10 +19,11 @@ async def task_notify_inactive_users():
     Send notification to pass quiz when new attempt is available.
     """
     logger.info("Running notify_inactive_users task.")
-    notification_service = NotificationService(
-        notification_repository=NotificationRepository(pg_helper.session_getter()),
-        quiz_repository=QuizRepository(pg_helper.session_getter()),
-        company_repository=CompanyRepository(pg_helper.session_getter()),
-        quiz_participation_repository=QuizParticipationRepository(pg_helper.session_getter())
-    )
-    await notification_service.notify_inactive_users()
+    async with pg_helper.session_factory() as session:
+        notification_service = NotificationService(
+            notification_repository=NotificationRepository(session),
+            quiz_repository=QuizRepository(session),
+            company_repository=CompanyRepository(session),
+            quiz_participation_repository=QuizParticipationRepository(session)
+        )
+        await notification_service.notify_inactive_users()
